a6adcc584d70f0b4d03aa492f894c4c9c22aa895
mauriciodiaz99 <dwdmmadm2001@gmail.com>
2022-12-24 15:59:03 -0600

    CRUD y Seccion 4 Terminada

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Car } from './interfaces/car.interface';
 import { v4 as uuid } from 'uuid';
+
+import { CreateCarDto, UpdateCarDto } from './dto';
+import { Car } from './interfaces/car.interface';
 
 @Injectable()
 export class CarsService {
@@ -30,5 +32,38 @@ export class CarsService {
         const car = this.cars.find(car => car.id === id);
         if ( !car ) throw new NotFoundException(`Car with id '${id}' not found`);
         return car;
+    }
+
+    create( createCarDto: CreateCarDto ) {
+        const car: Car = {
+            id: uuid(),
+            ...createCarDto
+        }
+        this.cars.push(car);
+
+        return car;
+    }
+
+    update( id: string, updateCarDto: UpdateCarDto ) {
+        
+        let carDB = this.findOneById(id);
+        this.cars = this.cars.map( car => {
+            if( car.id === id) {
+                carDB = {
+                    ...carDB,
+                    ...updateCarDto,
+                    id,
+                }
+                return carDB;
+            } 
+            return car;
+        } )
+        return carDB;
+
+    }
+
+    delete( id: string) {
+        const carDb = this.findOneById(id);
+        this.cars = this.cars.filter( car => car.id !== id );
     }
 }
---
+++ src/cars/dto/index.ts
@@ -0,0 +1,2 @@
+export { CreateCarDto } from "./create-car.dto";
+export { UpdateCarDto } from "./update-cr.dto";
